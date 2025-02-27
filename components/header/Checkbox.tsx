4f6fbd699c73b92c50978d770d2c7865d8779cf9
zntb <zntbdev@proton.me>
2025-02-27 11:39:39 +0200

    Hide checkbox if user is not authenticated

--- components/header/Checkbox.tsx
+++ components/header/Checkbox.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useCurrentUser } from '@/hooks/useSession';
 import classes from './main-header.module.css';
 
 type CheckboxProps = {
@@ -8,6 +9,9 @@ type CheckboxProps = {
 };
 
 const Checkbox = ({ isChecked, onCheckboxChange }: CheckboxProps) => {
+  const user = useCurrentUser();
+
+  if (!user) return null;
   return (
     <label
       className={`${classes.checkbox} ${isChecked ? classes.checked : ''}`}
