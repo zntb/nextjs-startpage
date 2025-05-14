62111afbf3a15fe60d21d46bd8956e05c597b985
zntb <zntbdev@proton.me>
2025-05-14 09:45:10 +0300

    Update prisma, package.json

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '../generated/prisma';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
